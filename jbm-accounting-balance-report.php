77f2b19cfd0637f54833c834be47da063ae28b69
j0sh9 <jbphotoaz@gmail.com>
2017-07-21 20:56:07 -0700

    Update jbm-accounting-balance-report.php

--- jbm-accounting-balance-report.php
+++ jbm-accounting-balance-report.php
@@ -37,19 +37,6 @@ function jbm_accounting_report_html() {
 	} else { 
 		$end_date = date('Y-m-d', strtotime($start_date));
 	}
-	
-	$completed_check = $processing_check = $pending_check = $on_hold_check = $refunded_check = $cancelled_check = $failed_check = '';
-	if ( isset($_POST["order_status"]) ) {
-		foreach ( $_POST["order_status"] as $check_status ) {
-			$varvar = str_replace('-','_',$check_status).'_check';
-			${$varvar} = 'checked';
-			$order_statuss[] = 'wc-'.$check_status;
-		}
-	} else {
-		$completed_check = 'checked';
-		$completed = 'completed';
-		$order_statuss = array('wc-completed');
-	}
 		
 ?>
 <style>
@@ -69,19 +56,12 @@ function jbm_accounting_report_html() {
 	}
 </style>
 
-<h1>Basic Order Report</h1>
+<h1>Balance Order Report</h1>
 <div>
 <form action="" method="post" class="basic_order_search_form">
 	<label>Start Date: <input type="date" name="start_date" id="start_date" value="<?=$start_date;?>"   /></label> Order Creation Date
 	<br>
 	<label>End Date: <input type="date" name="end_date" id="end_date" value="<?=$end_date;?>"   /></label><br>
-	<label><input type="checkbox" name="order_status[]" value="completed" <?=$completed_check?> /> Completed</label> | 
-	<label><input type="checkbox" name="order_status[]" value="processing" <?=$processing_check?> /> Processing</label> | 
-	<label><input type="checkbox" name="order_status[]" value="pending" <?=$pending_check?> /> Pending</label> | 
-	<label><input type="checkbox" name="order_status[]" value="on-hold" <?=$on_hold_check?> /> On Hold</label> | 
-	<label><input type="checkbox" name="order_status[]" value="refunded" <?=$refunded_check?> /> Refunded</label> | 
-	<label><input type="checkbox" name="order_status[]" value="cancelled" <?=$cancelled_check?> /> Cancelled</label> | 
-	<label><input type="checkbox" name="order_status[]" value="failed" <?=$failed_check?> /> Failed</label>
 	<p><button class="button" type="submit" name="jbm_order_query" value="1" >Search Orders</button></p>
 </form>	
 </div>
