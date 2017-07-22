060cfdd27572e5380e6df74a32669ab9eff87616
j0sh9 <jbphotoaz@gmail.com>
2017-07-21 17:58:52 -0700

    Update get-reports.php

--- get-reports.php
+++ get-reports.php
@@ -62,7 +62,7 @@ if ( isset($start_date) ) {
 	$start_date = date('Y-m-d 00:00:00', strtotime($start_date));
 	$end_date = date('Y-m-d 00:00:00', strtotime($end_date.' + 1 days'));
 } else {
-	$start_date = date('Y-m-d 00:00:00', strtotime(current_time('mysql')));
+	$start_date = date('Y-m-d 00:00:00', strtotime(current_time('mysql').' -1 days'));
 	$end_date = date('Y-m-d 00:00:00', strtotime($start_date.' +1 days'));
 }
 $order_rows = '';
