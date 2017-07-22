6789abe1ecb7bdf4542e4f1a4ebeeac79401922c
j0sh9 <jbphotoaz@gmail.com>
2017-07-21 22:31:01 -0700

    Update get-reports.php

--- get-reports.php
+++ get-reports.php
@@ -77,7 +77,7 @@ foreach($payment_methods as $payment_method) {
 	$results = $wpdb->get_results( $select, OBJECT );
 
 	//if ( count($results) > 0 ) {
-		$order_rows .= jbm_accounting_report_get_rows($results, $payment_method); 
+	$order_rows .= jbm_accounting_report_get_rows($results, $payment_method); 
 	//}
 }
 	
