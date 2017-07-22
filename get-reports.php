371b3180c140cd21aac144f9551b3d6108dbeeb3
j0sh9 <jbphotoaz@gmail.com>
2017-07-21 19:54:40 -0700

    Update get-reports.php

--- get-reports.php
+++ get-reports.php
@@ -57,7 +57,7 @@ function jbm_accounting_report_get_rows($results,$payment_method) {
 }
 
 global $wpdb;
-if ( isset($_GET['start_date']) ) $start_date = $_GET['start_date'];
+if ( isset($_GET['start_date']) ) $start_date = urldecode($_GET['start_date']);
 if ( isset($start_date) ) {
 	$start_date = date('Y-m-d 00:00:00', strtotime($start_date));
 	$end_date = date('Y-m-d 00:00:00', strtotime($start_date.' + 1 days'));
