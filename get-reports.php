127400f3a521387f8a76ded5e526071011566b4f
j0sh9 <jbphotoaz@gmail.com>
2017-07-21 17:16:39 -0700

    v0.9.0
    initial upload

---
+++ get-reports.php
@@ -0,0 +1,105 @@
+<?php
+function jbm_accounting_report_get_rows($results,$payment_method) {
+
+	$order_rows = "";
+	$subtotal = 0;
+	$shipping_total = 0;
+	$total_tax = 0;
+	$discount_total = 0;
+	$total = 0;
+	foreach($results as $order_key => $order_id) {
+		$customer_order = wc_get_order($order_id->post_id);
+
+		$order_date = wc_format_datetime( $customer_order->get_date_created(), 'Y-m-d H:i');
+		if ( ! empty($customer_order->get_date_paid()) ) 
+			$date_paid = wc_format_datetime( $customer_order->get_date_paid(), 'Y-m-d H:i' );
+		else $date_paid = '';
+		if ( ! empty($customer_order->get_date_completed()) ) 
+			$date_completed = wc_format_datetime( $customer_order->get_date_completed(), 'Y-m-d H:i' );
+		else $date_completed = '';
+		/*
+		$order_rows .= "
+		<tr>
+			<td><a href='/wp-admin/post.php?post=".$customer_order->get_id()."&action=edit' target='_blank'>".$customer_order->get_id()."</a></td>";
+		if( current_user_can('manage_options') ) $order_rows .= "<td><a href='/wp-admin/admin.php?page=jbm-generate-referrals&order_id=".$customer_order->get_id()."' target='_blank'>".$customer_order->get_id()."</a></td>";
+		$order_rows .= "
+			<td>".$order_date."</td>
+			<td>".$date_paid."</td>
+			<td>".$date_completed."</td>
+			<td><a href='/wp-admin/edit.php?post_status=all&post_type=shop_order&_customer_user=".$customer_order->get_customer_id()."' target='_blank'>".$customer_order->get_billing_first_name().' '.$customer_order->get_billing_last_name()."</a></td>
+			<td>".$customer_order->get_status()."</td>
+			<td>".$customer_order->get_payment_method()."</td>
+			<td>".number_format($customer_order->get_subtotal(),2)."</td>
+			<td>".number_format($customer_order->get_shipping_total(),2)."</td>
+			<td>".number_format($customer_order->get_total_tax(),2)."</td>
+			<td>".number_format($customer_order->get_discount_total(),2)."</td>
+			<td>".number_format($customer_order->get_total(),2)."</td>
+		</tr>";
+		*/
+		$subtotal += $customer_order->get_subtotal();
+		$shipping_total += $customer_order->get_shipping_total();
+		$total_tax += $customer_order->get_total_tax();
+		$discount_total += $customer_order->get_discount_total();
+		$total += $customer_order->get_total();
+	}
+	$order_rows .= "
+	<tr>
+		<th>".$customer_order->get_payment_method_title()."s</th>
+		<td>".count($results)."</td>
+		<td>".number_format($subtotal,2)."</td>
+		<td>".number_format($shipping_total,2)."</td>
+		<td>".number_format($total_tax,2)."</td>
+		<td>".number_format($discount_total,2)."</td>
+		<td>".number_format($total,2)."</td>
+	</tr>";
+
+	return $order_rows;
+}
+
+global $wpdb;
+
+if ( isset($start_date) ) {
+	$start_date = date('Y-m-d 00:00:00', strtotime($start_date));
+	$end_date = date('Y-m-d 00:00:00', strtotime($end_date.' + 1 days'));
+} else {
+	$start_date = date('Y-m-d 00:00:00');
+	$end_date = date('Y-m-d 00:00:00', strtotime($start_date.' +1 days'));
+}
+$order_rows = '';
+$gateways = new WC_Payment_Gateways;
+$payment_methods = $gateways->get_payment_gateway_ids();
+foreach($payment_methods as $payment_method) {
+	$select = "SELECT post_id FROM wp_postmeta
+	WHERE ";
+	$select .= "meta_key LIKE '_paid_date' AND DATE(meta_value) BETWEEN '$start_date' AND '$end_date' ";
+	$select .= "
+	AND post_id IN (SELECT ID FROM wp_posts WHERE post_status = 'wc-completed' OR post_status = 'wc-processing')";
+	$select .= "
+	AND post_id IN (SELECT post_id FROM wp_postmeta WHERE meta_key = '_payment_method' AND meta_value = '$payment_method')";
+
+	$results = $wpdb->get_results( $select, OBJECT );
+
+	if ( count($results) > 0 ) {
+		$order_rows .= jbm_accounting_report_get_rows($results, $payment_method); 
+	}
+}
+	
+?>
+<div class="balance-report">
+	<table class="jb-affiliate-report">
+		<thead>
+			<tr>
+				<th>Payment Method</th>
+				<th>Orders</th>
+				<th>Sub Total</th>
+				<th>Shipping</th>
+				<th>Tax</th>
+				<th>Discounts</th>
+				<th>Total</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?=$order_rows?>
+		</tbody>
+	</table>
+</div>
---
+++ jbm-accounting-balance-report.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Plugin Name: _Accounting Report
+Description: Accounting Report Page
+Version: 1.0
+*/
+
+function jbm_accounting_report_faux_api() {
+  if ( !isset($_GET['jbm_accounting_report_faux_api']) ) return;
+
+  include plugin_dir_path( __FILE__ )."get-reports.php";
+  die();
+}
+add_action( 'template_redirect', 'jbm_accounting_report_faux_api' );
+
+add_action( 'admin_menu', 'jbm_accounting_report' );
+function jbm_accounting_report() {
+	$jb_parent_slug = 'woocommerce';
+	$jb_page_title = 'Daily Accounting Report';
+	$jb_menu_title = 'Accounting Report';
+	$jb_capability = 'manage_options';
+	$jb_menu_slug = 'jbm-accounting-report';
+	$jb_callback = 'jbm_accounting_report_html';
+	//$jb_icon_url = 'dashicons-media-spreadsheet';
+	//$jb_menu_position = 120;
+	add_submenu_page(  $jb_parent_slug, $jb_page_title,  $jb_menu_title,  $jb_capability,  $jb_menu_slug,  $jb_callback );
+}
+
+function jbm_accounting_report_html() {
+	if ( isset($_POST['start_date']) ) {
+		$start_date = $_POST['start_date'];
+	} else { 
+		$start_date = date('Y-m-d', strtotime('-1 days'));
+	}
+	if ( isset($_POST['end_date']) ) {
+		$end_date = $_POST['end_date'];
+	} else { 
+		$end_date = date('Y-m-d', strtotime('today'));
+	}
+	
+	$completed_check = $processing_check = $pending_check = $on_hold_check = $refunded_check = $cancelled_check = $failed_check = '';
+	if ( isset($_POST["order_status"]) ) {
+		foreach ( $_POST["order_status"] as $check_status ) {
+			$varvar = str_replace('-','_',$check_status).'_check';
+			${$varvar} = 'checked';
+			$order_statuss[] = 'wc-'.$check_status;
+		}
+	} else {
+		$completed_check = 'checked';
+		$completed = 'completed';
+		$order_statuss = array('wc-completed');
+	}
+		
+?>
+<style>
+	.jb-affiliate-report, .jb-affiliate-report th, .jb-affiliate-report td {
+		border: 1px solid #cdcdcd;
+		border-collapse: collapse;
+	}
+	.jb-affiliate-report {
+		width: 97%;
+		margin: 3vw 1vw;
+	}
+	.jb-affiliate-report th, .jb-affiliate-report td {
+		padding: 4px 8px;
+	}
+	.basic_order_search_form label {
+		padding: 3px;
+	}
+</style>
+
+<h1>Basic Order Report</h1>
+<div>
+<form action="" method="post" class="basic_order_search_form">
+	<label>Start Date: <input type="date" name="start_date" id="start_date" value="<?=$start_date;?>"   /></label> Order Creation Date
+	<br>
+	<label>End Date: <input type="date" name="end_date" id="end_date" value="<?=$end_date;?>"   /></label><br>
+	<label><input type="checkbox" name="order_status[]" value="completed" <?=$completed_check?> /> Completed</label> | 
+	<label><input type="checkbox" name="order_status[]" value="processing" <?=$processing_check?> /> Processing</label> | 
+	<label><input type="checkbox" name="order_status[]" value="pending" <?=$pending_check?> /> Pending</label> | 
+	<label><input type="checkbox" name="order_status[]" value="on-hold" <?=$on_hold_check?> /> On Hold</label> | 
+	<label><input type="checkbox" name="order_status[]" value="refunded" <?=$refunded_check?> /> Refunded</label> | 
+	<label><input type="checkbox" name="order_status[]" value="cancelled" <?=$cancelled_check?> /> Cancelled</label> | 
+	<label><input type="checkbox" name="order_status[]" value="failed" <?=$failed_check?> /> Failed</label>
+	<p><button class="button" type="submit" name="jbm_order_query" value="1" >Search Orders</button></p>
+</form>	
+</div>
+<?php
+			include plugin_dir_path( __FILE__ )."get-reports.php";
+}
+
